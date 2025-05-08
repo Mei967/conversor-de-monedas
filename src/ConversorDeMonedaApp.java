3627266cd547ace1f683fdb5337c2872db482c0d
Meiby Burgos <meibyburgosa@gmail.com>
2025-05-08 00:41:41 -0400

    Primera versión del menú del conversor de monedas

---
+++ src/ConversorDeMonedaApp.java
@@ -0,0 +1,54 @@
+
+import java.util.Scanner;
+
+public class ConversorDeMonedaApp {
+
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("***************************************");
+
+        System.out.println("Bienvenido al conversor de monedas");
+
+        //menu
+        System.out.println("""
+                1 Dólar =>> Peso Chileno
+                2 Peso Chileno =>> Dólar\s
+                3 Dólar =>> Real brasileño\s
+                4 Real brasileño =>> Dólar\s
+                5 Dólar =>> Peso colombiano
+                6 Peso colombiano =>> Dólar\s
+                7 salir""");
+
+        System.out.println("************************************");
+
+        while (true) {
+             System.out.println("Elija una opción válida entre 1 y 7: ");
+            if (teclado.hasNextInt()) { //validación de entrada (solo números)
+                int opcion = teclado.nextInt();
+                if (opcion >= 1 && opcion <= 7 ){
+                    break;
+                }
+                else{
+                    System.out.println("Número incorrecto. Intente nuevamente");
+                }
+            }
+            else {
+                System.out.println("Entrada inválida. Ingrese solo numeros");
+                teclado.next();
+            }
+
+
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
